6bee794774b8841a300e23d09614bac777d4b5c6
Florian Oswald <florian.oswald@gmail.com>
2025-04-07 17:38:46 +0200

    add renv and update paper

--- code/R/script.R
+++ code/R/script.R
@@ -1,33 +1,47 @@
-    # my R script
+# my R script
 
-    library(fixest)
-    library(data.table)
-    library(logger)
+root = "/Users/floswald/git/ReproWorkshop"
 
-    root = "/Users/floswald/git/ReproWorkshop"
+library(renv)
+renv::init(project = file.path(root,"code","R"))
 
-    log_info("loading data from csv")
+# Need to install packages only on first run
+# install.packages(c("fixest","data.table","logger","grateful"))
+# renv::snapshot()
 
-    csvtime = system.time( {d = fread(file.path(root,"data","raw","data.csv"))} )
+# those libraries come from the *local* project library
+library(fixest)
+library(data.table)
+library(logger)
 
-    m = list()  # model collection list
-    
-    log_info("running fixest models")
-    fetime = system.time( {
-        m[["(0)"]] = feols(y ~ x1 + x2 , data = d)
-        m[["(1)"]] = feols(y ~ x1 + x2 | id1, data = d)
-        m[["(2)"]] = feols(y ~ x1 + x2 | id1 + id2, data = d)
-        m[["(3)"]] = feols(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 | id1 + id2, data = d)
-    })
-    etable(m[["(0)"]],m[["(1)"]],m[["(2)"]],m[["(3)"]], tex = TRUE, replace = TRUE, file = file.path(root,"output","tables","table2.tex"))
+log_info("R: now running in an `renv`!")
 
-    pdf(file.path(root,"output","plots","figure1.pdf"))
-    coefplot(m)
-    dev.off()
+log_info("loading data from csv")
 
-    log_info("R timings:")
-    log_info("reading csv: {csvtime['elapsed']} seconds")
-    log_info("running FE : {fetime['elapsed']} seconds")
+csvtime = system.time( {d = fread(file.path(root,"data","raw","data.csv"))} )
 
-    log_info("R: done")
-    
+m = list()  # model collection list
+
+log_info("running fixest models")
+fetime = system.time( {
+    m[["(0)"]] = feols(y ~ x1 + x2 , data = d)
+    m[["(1)"]] = feols(y ~ x1 + x2 | id1, data = d)
+    m[["(2)"]] = feols(y ~ x1 + x2 | id1 + id2, data = d)
+    m[["(3)"]] = feols(y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 | id1 + id2, data = d)
+})
+etable(m[["(0)"]],m[["(1)"]],m[["(2)"]],m[["(3)"]], tex = TRUE, replace = TRUE, file = file.path(root,"output","tables","table2.tex"))
+
+pdf(file.path(root,"output","plots","figure1.pdf"))
+coefplot(m)
+dev.off()
+
+log_info("R timings:")
+log_info("reading csv: {csvtime['elapsed']} seconds")
+log_info("running FE : {fetime['elapsed']} seconds")
+
+# Write package citations to paper and readme
+grateful::get_pkgs_info(pkgs = "Session",output = "citekeys",  out.dir = file.path(root,"paper"), dependencies = TRUE)
+grateful::cite_packages(output = "file", out.format = "md",pkgs = "Session", out.dir = root, dependencies = TRUE)
+
+log_info("R: done")
+
